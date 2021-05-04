7037481abd0666bed4a684fd8a5342f108748f9a
Louie Crook <55953362+LouieCrook@users.noreply.github.com>
2021-05-04 19:02:06 +0100

    navigation_bar
    updates to the navigation bar everything working.
    
    todo - issue with having to double click bottomNav buttons x2 to switch pages

--- app/src/main/java/com/louie/warzonestats/MainActivity.kt
+++ app/src/main/java/com/louie/warzonestats/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.louie.warzonestats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.louie.warzonestats.fragments.FaveFragment
+import com.louie.warzonestats.fragments.HomeFragment
+import com.louie.warzonestats.fragments.LeaderboardFragment
+import com.louie.warzonestats.fragments.StreamerFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val leaderboardFragment = LeaderboardFragment()
+        val streamerFragment = StreamerFragment()
+        val faveFragment = FaveFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        val bottomNavigation = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemReselectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_leaderboard -> makeCurrentFragment(leaderboardFragment)
+                R.id.ic_streamer -> makeCurrentFragment(streamerFragment)
+                R.id.ic_fave -> makeCurrentFragment(faveFragment)
+            }
+            true
+        }
+
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_wrapper, fragment)
+            commit()
+        }
 }
