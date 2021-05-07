920346416e1ef5ca0e95239f69e877e6325ca406
Louie Crook <55953362+LouieCrook@users.noreply.github.com>
2021-05-07 17:59:10 +0100

    Weekly & Lifetime Stats

--- app/src/main/java/com/louie/warzonestats/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/louie/warzonestats/ui/profile/ProfileActivity.kt
@@ -24,22 +24,27 @@ class ProfileActivity : AppCompatActivity() {
 
         // assign variables to required model directories
         var lifetimeStatsBR = playerModel!!.data.lifetime.mode.br.properties
+        var weeklyStatsBR = playerModel!!.data.weekly.mode.br_all.properties
         var user_battle = playerModel!!.battleNetId
         var user_xbl = playerModel!!.xblId
         var user_psn = playerModel!!.psnId
         var username = ""
         var kd_league = ""
 
-        // assign variables to data values needed to present lifetime player stats
+        // lifetime player stats - assign data to variables
         var lifetime_KD = String.format("%.2f", lifetimeStatsBR.kdRatio).toDouble()
         var lifetime_Wins = lifetimeStatsBR.wins
         var lifetime_Kills = lifetimeStatsBR.kills
-        var lifetime_WinRate = (lifetimeStatsBR.gamesPlayed / lifetimeStatsBR.wins).toString()
-        var lifetime_KillsPerGame = String.format(
-            "%.2f",
-            lifetimeStatsBR.kills.toDouble() / lifetimeStatsBR.gamesPlayed.toDouble()
-        )
+        var lifetime_WinRate = String.format("%.2f", lifetimeStatsBR.gamesPlayed.toDouble() / lifetimeStatsBR.wins.toDouble())
+        var lifetime_KillsPerGame = String.format("%.2f", lifetimeStatsBR.kills.toDouble() / lifetimeStatsBR.gamesPlayed.toDouble())
 
+        // weekly player stats - assign data to variables
+        var weekly_KD = String.format("%.2f", weeklyStatsBR.kdRatio).toDouble()
+        var weekly_KillsPerGame = String.format("%.2f", weeklyStatsBR.killsPerGame).toDouble()
+        var weekly_Kills = weeklyStatsBR.kills
+        var weekly_Matches = weeklyStatsBR.matchesPlayed
+
+        // todo - can probably change this logic once I have the platform buttons working
         // logic to set username to correct value and not null
         if (user_battle == null && user_psn == null) {
             username = user_xbl as String
@@ -64,34 +69,51 @@ class ProfileActivity : AppCompatActivity() {
 
         // LIFETIME STATS
             // KD
-                // set player lifetime KD to textView within ProfileActivity
+                // set player lifetime KD to textView
                 var viewLifetimeKD = findViewById<View>(R.id.lifetime_kd_text) as TextView
                 viewLifetimeKD.text = lifetime_KD.toString()
-
-                // set player lifetime KD to textView within ProfileActivity
+                // set player lifetime KD to textView
                 var viewLeagueKD = findViewById<View>(R.id.league_kd_text) as TextView
                 viewLeagueKD.text = kd_league.capitalize()
-
                 // set background plate as drawable league gradient
                 var viewLeagueKD_box = findViewById<View>(R.id.box_kd) as TextView
                 viewLeagueKD_box.background = ContextCompat.getDrawable(this@ProfileActivity, R.drawable.box_diamond)
-
-
             // WINS
-
+                // set player lifetime wins to textView
+                var viewLifetimeWins = findViewById<View>(R.id.lifetime_wins_text) as TextView
+                viewLifetimeWins.text = lifetime_Wins.toString()
             // KILLS
-
+                // set player lifetime kills to textView
+                var viewLifetimeKills = findViewById<View>(R.id.lifetime_kills_text) as TextView
+                viewLifetimeKills.text = lifetime_Kills.toString()
             // WIN %
-
+                // set player lifetime win percentage to textView
+                var viewLifetimeWinrate = findViewById<View>(R.id.lifetime_winrate_text) as TextView
+                viewLifetimeWinrate.text = lifetime_WinRate.toString()
             // KILLS PER GAME
+                // set player lifetime win percentage to textView
+                var viewLifetimeKPG = findViewById<View>(R.id.lifetime_killsPerGame_text) as TextView
+                viewLifetimeKPG.text = lifetime_KillsPerGame.toString()
 
         // WEEKLY STATS
+        // todo - add logic that compares weekly stats to lifetime
+        // todo - red box colour = worse performance, green box colour = better performance
+            // MATCHES PLAYED
+                // set player weekly matches played to textView
+                var viewWeeklyMatches = findViewById<View>(R.id.weeklyMatches_text) as TextView
+                viewWeeklyMatches.text = weekly_Matches.toString()
             // KD
-
+                // set player weekly matches played to textView
+                var viewWeeklyKD = findViewById<View>(R.id.weeklyKD_text) as TextView
+                viewWeeklyKD.text = weekly_KD.toString()
             // KILLS PER GAME
-
+                // set player weekly matches played to textView
+                var viewWeeklyKPG = findViewById<View>(R.id.weeklyKPG_text) as TextView
+                viewWeeklyKPG.text = weekly_KillsPerGame.toString()
             // KILLS
-
+                // set player weekly matches played to textView
+                var viewWeeklyKills = findViewById<View>(R.id.weeklyKills_text) as TextView
+                viewWeeklyKills.text = weekly_Kills.toString()
 
 
     }
