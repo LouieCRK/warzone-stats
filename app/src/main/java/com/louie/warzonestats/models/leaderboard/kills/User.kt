ff25c64ff68d9443350c70d9325bf1f9e0ac82af
Louie Crook <55953362+LouieCrook@users.noreply.github.com>
2021-05-12 13:01:50 +0100

    Revert "Revert "Leaderboard Wins - Model""
    This reverts commit 633437a041ea03eb9eac03db22f74533db2575d6.

---
+++ app/src/main/java/com/louie/warzonestats/models/leaderboard/kills/User.kt
@@ -0,0 +1,10 @@
+package com.louie.warzonestats.models.leaderboard.kills
+
+data class User(
+    val battle: Any,
+    val platform: String,
+    val premiumLevel: Any,
+    val psn: Any,
+    val uno: String,
+    val xbl: String
+)
---
+++ app/src/main/java/com/louie/warzonestats/services/LeaderboardKillsService.kt
@@ -0,0 +1,7 @@
+package com.louie.warzonestats.services
+
+import com.louie.warzonestats.models.leaderboard.kills.LeaderboardKillsModel
+
+object LeaderboardKillsService {
+    var currentLeaderboardKillsModel : LeaderboardKillsModel? = null
+}
